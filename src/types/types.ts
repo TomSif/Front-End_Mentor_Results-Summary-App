dea2c45313a5a3a2154b152331d6c1eb9b043212
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-18 12:17:57 +0200

    feat(types): add Feedback interface

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,16 @@ export interface Result {
   score: number;
   icon: string;
 }
+
+export type ResultLevel =
+  | "bad" // 00-24
+  | "poor" // 25-49
+  | "fair" // 50-74
+  | "great"; // 75-100
+
+export interface Feedback {
+  min: number;
+  max: number;
+  level: ResultLevel;
+  message: string;
+}
