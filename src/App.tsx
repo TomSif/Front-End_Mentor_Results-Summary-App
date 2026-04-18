576b140a61aeb4db41faee5a9091796d79973b68
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-18 10:04:01 +0200

    chore(setup): init Vite React TypeScript Tailwind clsx tailwind-merge

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="min-h-screen grid place-items-center bg-slate-100">
+      <p className="text-slate-600">Results Summary Component</p>
+    </main>
+  )
+}
+
+export default App
