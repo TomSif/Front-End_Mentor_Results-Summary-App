9166516a8dae395df8115c2b93afdc6f54a8fbb0
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-19 12:13:39 +0200

    feat(app): handle Result state

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,27 @@ function App() {
   );
   //useState to stock scores by categories
   const [scores, setScores] = useState(scoresByCategories);
+  // store the score result
+  const [scoreResult, setScoreResult] = useState(() => {
+    // calcul the result by default
+    const startValues = Object.values(scoresByCategories);
+    const startResult =
+      startValues.reduce((acc, val) => acc + val, 0) / startValues.length;
+    return startResult;
+  });
+
   return (
     <main className="min-h-screen grid place-items-center bg-white w-full">
       <form
         className="w-full  "
         onSubmit={(e) => {
           e.preventDefault();
+          // calcul the Result
+          const values = Object.values(scores);
+          const result =
+            values.reduce((acc, val) => acc + val, 0) / values.length;
+          // set Result Score
+          setScoreResult(result);
         }}
       >
         <ul className="w-full">
