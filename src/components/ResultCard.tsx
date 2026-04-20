a9228082d554d2af13a4b1fa34752a22e76b46d9
TomSif <thomas.sifferle.programmer@gmail.com>
2026-04-20 11:00:47 +0200

    feat(ui): add animated score counter from 0 to final value

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import getFeedback from "../utils/getFeedback";
 
 interface ResultCardProps {
@@ -5,6 +6,22 @@ interface ResultCardProps {
 }
 
 const ResultCard = ({ score }: ResultCardProps) => {
+  const [dispayScore, setDisplayScore] = useState(0);
+  // Animation Display Score
+  useEffect(() => {
+    setDisplayScore(0);
+    const id = setInterval(() => {
+      setDisplayScore((prev) => {
+        if (prev >= score) {
+          clearInterval(id);
+          return prev;
+        }
+        return prev + 1;
+      });
+    }, 10);
+    return () => clearInterval(id);
+  }, [score]);
+  // FeedBack for level and comment
   const feedback = getFeedback(score);
   return (
     <section className="bg-linear-to-b from-gradient-2-from to-gradient-2-to  w-full md:max-w-84.5 xl:max-w-92 py-8 md:py-11 mx-auto rounded-b-4xl md:rounded-4xl md:flex  ">
@@ -13,7 +30,7 @@ const ResultCard = ({ score }: ResultCardProps) => {
           Your Result
         </h1>
         <div className="flex flex-col font-bold items-center justify-center gap-2 rounded-full w-35 h-35 md:w-50 md:h-50 bg-linear-to-b from-gradient-1-from to-gradient-1-to text-preset-2 md:text-preset-1 text-white">
-          {Math.floor(score)}
+          {dispayScore}
           <span className="text-preset-6 font-bold text-navy-200">of 100</span>
         </div>
         <div className="flex flex-col items-center text-center gap-2 md:gap-4">
